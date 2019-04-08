2b84622200a693e2643964efd3fe2675ed605290
github username <github email address>
2019-04-08 11:04:32 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,17 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+  appendUser()
 });
+
+function fetchUser() {
+  return fetch('https://randomuser.me/api/')
+  .then(response => response.json())
+}
+
+function appendUser() {
+  fetchUser()
+  .then(data => {
+    console.log(data.results)
+  })
+}
